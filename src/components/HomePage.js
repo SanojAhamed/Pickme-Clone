4b60e12b8dbf58a2a33a47f385a0aa407a4c1d38
Sanoj Ahamed <sanojahamed1730@gmail.com>
2025-01-13 09:44:49 +0530

    Initial Update and Navbar update

---
+++ src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="bg-yellow-500 text-white">
+      <div className="max-w-7xl mx-auto px-4 py-16 sm:px-6 lg:px-8">
+        <h1 className="text-4xl font-extrabold text-center">
+          22,400 TIMES AROUND THE WORLD
+        </h1>
+        <p className="mt-4 text-lg text-center">
+          We have enabled over 900 million kilometers of travel across Sri Lanka through our technology, taking care of the daily commutes and activities of millions of people.
+        </p>
+        <p className="mt-2 text-lg text-center">
+          We add millions more kilometers every day!
+        </p>
+       
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customYellow: '#fdb940', // Add your custom color here
+      },
+    },
+  },
+  plugins: [],
+};
